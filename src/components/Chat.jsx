e3ddc7672452ea8a76c73b261f5fb272d394cfa3
Pedro Cézar <pedrocssolza@gmail.com>
2024-10-05 21:04:50 -0300

    up

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -18,7 +18,7 @@ function Chat() {
       setIsLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8000/chat", {
+        const response = await fetch("https://adams42.up.railway.app/chat", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
